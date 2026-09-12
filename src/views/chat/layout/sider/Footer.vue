878a7db554c3a3f8155ab0d60301a40f6f868fa2
liut <liutao@liut.cc>
2026-09-12 19:20:32 +0800

    feat(corpus): add keeper-only sider entry for corpus management

--- src/views/chat/layout/sider/Footer.vue
+++ src/views/chat/layout/sider/Footer.vue
@@ -1,10 +1,26 @@
 <script setup lang='ts'>
 import { defineAsyncComponent, ref } from 'vue'
+import { useRouter } from 'vue-router'
 import { HoverButton, SvgIcon, UserAvatar } from '@/components/common'
+import { useBasicLayout } from '@/hooks/useBasicLayout'
+import { t } from '@/locales'
+import { useAppStore, useAuthStore } from '@/store'
 
 const Setting = defineAsyncComponent(() => import('@/components/common/Setting/index.vue'))
 
+const router = useRouter()
+const appStore = useAppStore()
+const authStore = useAuthStore()
+const { isMobile } = useBasicLayout()
+
 const show = ref(false)
+
+function handleCorpusEntry() {
+  if (isMobile.value)
+    appStore.setSiderCollapsed(true)
+
+  router.push({ name: 'Corpus' })
+}
 </script>
 
 <template>
@@ -12,6 +28,12 @@ const show = ref(false)
     <div class="flex-1 flex-shrink-0 overflow-hidden">
       <UserAvatar />
     </div>
+
+    <HoverButton v-if="authStore.isKeeper" :tooltip="t('corpus.entry')" @click="handleCorpusEntry">
+      <span class="text-xl text-[#4f555e] dark:text-white">
+        <SvgIcon icon="ri:book-open-line" />
+      </span>
+    </HoverButton>
 
     <HoverButton v-if="true" @click="show = true">
       <span class="text-xl text-[#4f555e] dark:text-white">
